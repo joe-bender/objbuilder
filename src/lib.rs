9b8e662424f22c40f25b5ea0b5bc9b36f65e2af3
Joe Bender <joebenderweb@gmail.com>
2019-12-02 11:52:12 -0500

    Add function write_file

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,22 @@
+use std::error::Error;
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+
+pub fn write_file(path: &Path, contents: &str) {
+    let display = path.display();
+
+    let mut file = match File::create(&path) {
+        Err(err) => panic!("Couldn't create {}: {}", display, err.description()),
+        Ok(file) => file,
+    };
+
+    match file.write_all(contents.as_bytes()) {
+        Err(err) => panic!("Couldn't write to {}: {}", display, err.description()),
+        Ok(_) => println!("Wrote model to {}", display),
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
